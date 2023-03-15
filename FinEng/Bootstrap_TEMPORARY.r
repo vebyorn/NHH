ab687d96b396326d2cb45fd43ff99b43653640de
Vebjørn <119741691+vebyorn@users.noreply.github.com>
2023-03-15 22:36:07 +0100

    Updated

--- FinEng/Bootstrap_TEMPORARY.r
+++ FinEng/Bootstrap_TEMPORARY.r
@@ -1,42 +1,32 @@
-
-# ON = Overnight Rate
-# MM = Money Market Rate
-# FRA = Forward Rate Agreement
-# IRS = Interest Rate Swap
-bootstrapper = function(ON, MM, FRA, IRS, sigma) {
-    t = 0
-    Z = 1
-    # map interest futures onto FRAs by convexity
-    FRA = cnvx.adj(FRA, 1, 20, sigma)
-    # ON Deposits
-    Z = c(Z, 1 / ((1 + delta(, , 360) * ON[1]) * (1 + delta(, .242, 360) * ON[2])))
-    # MM Deposits
-    for {i in MM} {
-        Z = c(Z, Z / ((1 + delta(, , 360) * i) * (1 + delta(, , 360) * i)))
-    }
-
-}
-# Params
-ON = c(4.70, 4.57) # Overnight Rates: 1 week, 1 month
-MM = c(4.71, 4.84) # Money Market Deposits: 1 week, 1 month
-FRA = c(5.125, 5.376, 5.5260, 5.375, 5.6140) # Forward Rate Agreements: March 23, June 23, September 23, December 23, March 24
-IRS = c(4.9330, 4.5381, 4.2790, 4.1202, 3.9480, 3.8400, 3.8140, 3.7910, 3.7388) # Interest Rate Swaps: 2, 3, 4, 5, 7, 10, 12, 15, 20 years
-sigma = 0.006 # Volatility
-
 Z = 1
 t = 0
 
-# map interest futures onto FRAs by convexity
-FRA = cnvx.adj(FRA, 30, 380, sigma)
+# Overnight and Tomorrow Next Deposits
+ON = 0.047
+TN = 0.0457
 
-# ON Deposits
-Z = c(Z, 1 - (1 / ((1 + delta(0, 1, 360) * ON[1]) * (1 + delta(1, 30, 360) * ON[2]))))
+ON.dep = c(ON, TN)
+Z = c(Z, 1/((1 + (1/365.242) *  ON.dep[1]) * (1 + (3/365.242) * ON.dep[2]))) # ON and TN discount factors
+t = c(t, 4/365.242) # ON and TN accrual times
+
+# Money Market Deposits
+MM.w1 = 0.0471 # 1 week MM deposit
+MM.m1 = 0.0484 # 1 month MM deposit
+Z = c(Z, 1/((1 + (1/365.242) *  MM.w1) * (1 + (10/365.242) * MM.m1))) # 1 week and 1 month MM discount factors
+t = c(t, 11/365.242) # 1 week and 1 month MM accrual times
+
+# Forward Rate Agreements
+FRA.Mar23 = (100 - 94.93) / 100
+FRA.Jun23 = (100 - 94.57) / 100
+FRA.Sep23 = (100 - 94.485) / 100
+FRA.Dec23 = (100 - 94.72) / 100
+FRA.Mar24 = (100 - 95.15) / 100
+FRAs = c(FRA.Mar23, FRA.Jun23, FRA.Sep23, FRA.Dec23, FRA.Mar24)
+
+
+# Forward Rate Agreements Discount Factors
+t = c(t, 32/365.242, 118/365.242, 273/365.242) # 364/365.242, 455/365.242
+t
+Z = c(Z, 1/(1 + FRAs[1] * t[3]), 1/(1 + FRAs[2] * t[4]), 1/(1 + FRAs[3] * t[5]), 1/(1 + FRAs[4] * t[6]), 1/(1 + FRAs[5] * t[7]))
 Z
-
-# MM Deposits
-for (i in MM) {
-    Z = c(Z, 1 - Z / ((1 + delta(0, 1, 360) * i) * (1 + delta(1, 30, 360) * i)))
-}
-Z
-
-
+32 + 
