303001293f6aa77893e43b77e0bb78c3a102b83d
Vebjørn <119741691+vebyorn@users.noreply.github.com>
2023-03-16 20:37:33 +0100

    Update

--- FinEng/Bootstrap_TEMPORARY.r
+++ FinEng/Bootstrap_TEMPORARY.r
@@ -22,11 +22,40 @@ IF.Jun23 = (100 - 94.57) / 100
 IF.Sep23 = (100 - 94.485) / 100
 IF.Dec23 = (100 - 94.72) / 100
 IF.Mar24 = (100 - 95.15) / 100
-IFs = c(FRA.Mar23, FRA.Jun23, FRA.Sep23, FRA.Dec23, FRA.Mar24)
-
+IFs = c(IF.Mar23, IF.Jun23, IF.Sep23, IF.Dec23, IF.Mar24)
+IFs
 # times to maturity for each contract
 t = c(t, 32/365.242, 118/365.242, 216/365.242, 306/365.242, 397/365.242)
+t
 
 # turning IFs into futures rates
-IFs = (1 - IFs) / t[4:8]
-IFs
+# F = Future Price
+# f = Forward Rate Agreeement
+# sigma = short rate volatility
+# T1 = start date
+# T2 = end date
+cnvx.adj = function(F, T1, T2, sigma) {
+    f = F - (sigma^2 * T1 * T2) / 2
+    return (f)
+}
+
+# convert IFs into FRA rates
+FRAs = cnvx.adj(IFs, 0, t[4:8], 0.006)
+FRAs
+
+# convert FRA rates into discount factors
+Z = c(Z, Z/(1 + FRAs * t[4:8]))
+Z
+t
+
+## Interest Rate Swaps
+IRS.2y =
+IRS.3y =
+IRS.4y =
+IRS.5y =
+IRS.7y =
+IRS.10y =
+IRS.12y =
+IRS.15y =
+IRS.20y =
+
