1d3ccaa1310fa1f033d6ab9747a47e48e61aac99
Vebjørn <119741691+vebyorn@users.noreply.github.com>
2023-03-27 15:38:18 +0200

    Started on ir deriv

--- Interest Rate Derivatives.r
+++ Interest Rate Derivatives.r
@@ -3,7 +3,35 @@
 #####################
 rm(list=ls())
 
-## Libor Market Model (LMM / BGM) ##
-# We have to simulate the forward curve.
-# We need the instantaneous volatilities.
-# 
+# params
+L = c(0.01, 0.02, 0.03)
+A = c(0.2, 0.3)
+dt = 1
+N = 1
+K = 2
+M = 2
+
+A = matrix(A, nrow = 1, ncol = 2)
+L = matrix(L, nrow = 1, ncol = 3)
+
+# Simulate W
+set.seed(1)
+W = matrix(rnorm(N*M), nrow = N, ncol = M)
+
+i = 1
+k = 1
+m = 1
+Ai = A[i - k + 1]
+Ai
+
+Am = A[m - k + 1]
+Am
+
+Li = L[k - 1 + 1]
+Li
+
+mu = (dt * Li * Ai * Am) / (1 + dt * Li) - Am^2 / 2
+mu
+
+Lm = Li * exp(mu * dt + sqrt(dt) * Am * W)
+Lm
