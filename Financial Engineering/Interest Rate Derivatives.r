dd0344b5ec0ecfa787fa04518c9f7c9ad9929485
Vebjørn <119741691+vebyorn@users.noreply.github.com>
2023-03-30 20:55:22 +0200

    cleaned up comments

--- Financial Engineering/Interest Rate Derivatives.r
+++ Financial Engineering/Interest Rate Derivatives.r
@@ -14,44 +14,45 @@ rm(list=ls())
 # K = number of time steps
 # N = number of simulations
 lmm.sf = function(L, Lambda, dt, K, N) {
-  # Initialize output list
+  ## Initialize output list
   output = data.frame(k = numeric(), m = numeric(), Lambda = numeric(), `µm(Tk)` = numeric(), `W(Tk)` = numeric(), `Lm(Tk)` = numeric())  
 
-  # Loop for simulations
-  for (n in 1:N) {
-    L.prev = L[-1]
-    
-    # Loop for points in time (k marks the point in time we are valuing at)
-    for (k in 1:K) {
-      # Update L.prev to include the new value of Lm1(Tk) from previous iteration
+  ## Simulate Ns sims
+  for (n in 1:N) { # Loop over n simulations
+    L.prev = L[-1] # Previous libor rate
+
+    ## Simulate K time steps
+    for (k in 1:K) { # Loop for over time steps
       if (k == 1) {
-        L.prev = L[-1]
+        L.prev = L[-1] # Previous libor rate at time k = 1
       } else {
-        L.prev = c(L[k], L.prev[-1])
+        L.prev = c(L[k], L.prev[-1]) # Previous libor rate at time k > 1
       }
-      # Generate random number for W(Tk)
+
+      ## Simulate W(Tk)
       Wk = rnorm(1)
-      
-      # Loop for periods we are simulation at time k
-      for (m in k:(length(L) - 1)) {
-        sum.mu = 0
-        
-        # Loop to replicate sum function in formula
+
+      ## Simulate m periods at time k
+      for (m in k:(length(L) - 1)) { # Loop over m periods at time k
+        sum.mu = 0 # initialize first joint of mu
+
+        ## Sum over i = k to m for the first part of mu
         for (i in k:m) {
           sum.mu = sum.mu + dt * L.prev[i - k + 1] * Lambda[m - k + 1] * Lambda[i - k + 1] / (1 + dt * L.prev[i - k + 1])
         }
+
+        ## Calculate mu and Lm(Tk)
+        mu = sum.mu - Lambda[m - k + 1]^2 / 2 # calculate mu
+        L.new = L.prev[m - k + 1] * exp(mu * dt + sqrt(dt) * Lambda[m - k + 1] * Wk) # calculate new libor rate
         
-        # Calculate mu and L at time m
-        mu = sum.mu - Lambda[m - k + 1]^2 / 2
-        L.new = L.prev[m - k + 1] * exp(mu * dt + sqrt(dt) * Lambda[m - k + 1] * Wk)
-        
-        # Save output in output dataframe
+        ## Save output in output dataframe
         output = rbind(output, data.frame(k = k, m = m, Lambda = Lambda[m - k + 1], `µm(Tk)` = mu, `W(Tk)` = Wk, `Lm(Tk)` = L.new))
         L[m - k + 1] = L.new
       }
     }
   }
-  # Return Output dataframe
+  
+  ## Return Output dataframe
   return(output)
 }
 
@@ -61,4 +62,4 @@ Lambda <- c(0.20, 0.30) # Lambda = vector of stationary volatilities
 
 # Run simulation
 set.seed(1) # Set seed for reproducibility
-results = round(lmm.sf(L, Lambda, dt = 1, K = 2, N = 10), digits = 4); results
+results = round(lmm.sf(L, Lambda, dt = 1, K = 2, N = 10), digits = 4); results # print results
