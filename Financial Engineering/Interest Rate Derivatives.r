315ac667a6380c3abbc560a2bf2dbdde4b4a4e10
Vebjørn <119741691+vebyorn@users.noreply.github.com>
2023-03-31 18:47:15 +0200

    only one number left to fix

--- Financial Engineering/Interest Rate Derivatives.r
+++ Financial Engineering/Interest Rate Derivatives.r
@@ -62,7 +62,7 @@ Lambda = c(0.20, 0.30) # Lambda = vector of stationary volatilities
 
 # Run simulation
 set.seed(1) # Set seed for reproducibility
-results = round(lmm.sf(L, Lambda, dt = 1, K = 2, N = 10), digits = 4); results # print results
+results = round(lmm.sf(L, Lambda, dt = 1, K = 2, N = 10), digits = 4); results[1:3, ] # print results
 
 ###############################################################################################
 # initialise workspace
@@ -98,7 +98,7 @@ set.seed(1)
       if (k == 1) {
         L.prev = L[-1] # Previous libor rate at time k = 1
       } else {
-        L.prev = c(L[k], L.prev[-1]) # Previous libor rate at time k > 1
+        L.prev = c(L[k + 1]) # Previous libor rate at time k > 1
       }
 
       ## Simulate W(Tk) for each factor
@@ -106,10 +106,10 @@ set.seed(1)
 
       ## Simulate m periods at time k
       for (m in k:(length(L) - 1)) { # Loop over m periods at time k
-        sum.mu = 0 # initialize first joint of mu
-        sum.lam = 0
-        neg.mu = 0
-        Wkq = 0
+        sum.mu = 0 # initialising
+        sum.lam = 0 # the sum loop
+        neg.mu = 0 # variables
+        Wkq = 0 # and also W
 
         ## Sum over i = k to m for the first part of mu
         for (i in k:m) {
@@ -118,28 +118,36 @@ set.seed(1)
           for (q in 1:(length(L) - 1)) {
             sum.lam = Lambda[m - k + 1] * Lambda[i - k + 1]
           }
-          sum.mu = sum.mu + dt * L.prev[i - k + 1] * sum.lam / (1 + dt * L.prev[i - k + 1])
+          sum.mu = dt * L.prev[i - k + 1] * sum.lam / (1 + dt * L.prev[i - k + 1])
         }
 
-        ## Sum over q = 1 to p = M for the negative expression of mu
+        ## Sum over q = 1 to p = M
         for (q in 1:(length(L) - 1)) {
           neg.mu = Lambda[m - k + 1]^2 / 2
-        }
-
-        ## Sum over lambda * W for the final expression of Lm(Tk)
-        for (q in 1:(length(L) - 1)) {
           Wkq = Wk * Lambda[m - k + 1]
         }
 
         ## Calculate mu and Lm(Tk)
         mu = sum.mu - neg.mu # calculate mu
-        L.new = L.prev[m - k + 1] * exp(mu * dt + sqrt(dt) * Wkq) # calculate new libor rate
-        
+        L.new = L.prev[m - k + 1] * exp(mu * dt + sqrt(dt) * t(Wkq)) # calculate new libor rate
+
+        ## if k == m, then save L.new[1] in L.final else save L.new[2] in L.final
+        if (k == m) {
+          L.final = L.new[1]
+        } else {
+          L.final = L.new[2]
+        }
+
+
         ## Save output in output dataframe
-        output = rbind(output, data.frame(k = k, m = m, Lambda = Lambda[m - k + 1], `µm(Tk)` = mu, `W1(Tk)` = Wk[1], `W2(Tk)` = Wk[2], `Lm(Tk)` = L.new))
+        output = rbind(output, data.frame(k = k, m = m, Lambda = Lambda[m - k + 1], `µm(Tk)` = mu, `W1(Tk)` = Wk[1], `W2(Tk)` = Wk[2], `Lm(Tk)` = L.final))
       }
     }
   }
 
-  ## Return Output dataframe
-  round(output, 4)
+  ## Return first 3 rows of output dataframe
+  round(output[1:3, ], 4)
+
+# testing
+set.seed(1)
+
