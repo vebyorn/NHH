ffdac98ab4526f1211b8e09fa26b224760ea0ddb
Vebjørn <119741691+vebyorn@users.noreply.github.com>
2023-03-31 15:46:23 +0200

    getting closer to an answer

--- Financial Engineering/Interest Rate Derivatives.r
+++ Financial Engineering/Interest Rate Derivatives.r
@@ -68,15 +68,26 @@ results = round(lmm.sf(L, Lambda, dt = 1, K = 2, N = 10), digits = 4); results #
 # initialise workspace
 rm(list=ls())
 
-## Libor Market Model, Single Factor ##
-# L = vector of initial forward libor rates
-# Lambda = vector of stationary volatilities
-# dt = time step
-# K = number of time steps
-# N = number of simulations
-lmm.mf = function(L, Lambda, dt, K, N) {
-  ## Initialize output list
-  output = data.frame(k = numeric(), m = numeric(), Lambda = numeric(), `µm(Tk)` = numeric(), `W(Tk)` = numeric(), `Lm(Tk)` = numeric())  
+## Multifactor: Lambda Matrix Function ##
+# l1 = vector of volatilities for the first factor
+# l2 = vector of volatilities for the second factor
+mf.lambda = function (l1, l2) {
+  Lambda = sqrt(l1^2 + l2^2)
+  return(Lambda)
+}
+
+# Inputs
+L = c(0.01, 0.02, 0.03) # L = vector of initial forward libor rates
+l1 = c(0.2, 0); l2 = c(0, 0.3) # l1, l2 = vectors of volatilities
+Lambda = mf.lambda(l1, l2) # Lambda = matrix of volatilities
+dt = 1 # dt = time step
+K = 2 # K = number of time steps
+N = 10 # N = number of simulations
+
+## Initialize output list
+output = data.frame(k = numeric(), m = numeric(), Lambda = numeric(), `µm(Tk)` = numeric(), `W(Tk)` = numeric(), `Lm(Tk)` = numeric())  
+
+set.seed(1)
 
   ## Simulate Ns sims
   for (n in 1:N) { # Loop over n simulations
@@ -90,37 +101,45 @@ lmm.mf = function(L, Lambda, dt, K, N) {
         L.prev = c(L[k], L.prev[-1]) # Previous libor rate at time k > 1
       }
 
-      ## Simulate W(Tk)
-      Wk = rnorm(1)
+      ## Simulate W(Tk) for each factor
+      Wk = rnorm(length(Lambda))
 
       ## Simulate m periods at time k
       for (m in k:(length(L) - 1)) { # Loop over m periods at time k
         sum.mu = 0 # initialize first joint of mu
+        sum.lam = 0
+        neg.mu = 0
+        Wkq = 0
 
         ## Sum over i = k to m for the first part of mu
         for (i in k:m) {
-          sum.mu = sum.mu + dt * L.prev[i - k + 1] * Lambda[m - k + 1] * Lambda[i - k + 1] / (1 + dt * L.prev[i - k + 1])
+
+          ## Sum over q = 1 to M for the second part of mu
+          for (q in 1:(length(L) - 1)) {
+            sum.lam = Lambda[m - k + 1] * Lambda[i - k + 1]
+          }
+          sum.mu = sum.mu + dt * L.prev[i - k + 1] * sum.lam / (1 + dt * L.prev[i - k + 1])
+        }
+
+        ## Sum over q = 1 to p = M for the negative expression of mu
+        for (q in 1:(length(L) - 1)) {
+          neg.mu = Lambda[m - k + 1]^2 / 2
+        }
+
+        ## Sum over lambda * W for the final expression of Lm(Tk)
+        for (q in 1:(length(L) - 1)) {
+          Wkq = Wk * Lambda[m - k + 1]
         }
 
         ## Calculate mu and Lm(Tk)
-        mu = sum.mu - Lambda[m - k + 1]^2 / 2 # calculate mu
-        L.new = L.prev[m - k + 1] * exp(mu * dt + sqrt(dt) * Lambda[m - k + 1] * Wk) # calculate new libor rate
+        mu = sum.mu - neg.mu # calculate mu
+        L.new = L.prev[m - k + 1] * exp(mu * dt + sqrt(dt) * Wkq) # calculate new libor rate
         
         ## Save output in output dataframe
-        output = rbind(output, data.frame(k = k, m = m, Lambda = Lambda[m - k + 1], `µm(Tk)` = mu, `W(Tk)` = Wk, `Lm(Tk)` = L.new))
-        L[m - k + 1] = L.new
+        output = rbind(output, data.frame(k = k, m = m, Lambda = Lambda[m - k + 1], `µm(Tk)` = mu, `W1(Tk)` = Wk[1], `W2(Tk)` = Wk[2], `Lm(Tk)` = L.new))
       }
     }
   }
-  
+
   ## Return Output dataframe
-  return(output)
-}
-
-# Inputs
-L = c(0.01, 0.02, 0.03) # L = vector of initial forward libor rates
-Lambda =  # Lambda = vector of stationary volatilities
-
-# Run simulation
-set.seed(1) # Set seed for reproducibility
-results = round(lmm.mf(L, Lambda, dt = 1, K = 2, N = 10), digits = 4); results # print results
+  round(output, 4)
