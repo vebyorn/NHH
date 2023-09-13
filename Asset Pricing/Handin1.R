7c64cc7081ea19c7c50f1189e2e3cd34e2fff734
Vebjørn <119741691+vebyorn@users.noreply.github.com>
2023-09-13 11:35:51 +0200

    added comments

--- Asset Pricing/Handin1.R
+++ Asset Pricing/Handin1.R
@@ -9,17 +9,24 @@ c = exp(0.1 + (1-2)*0.4^2/2); c
 eOutcome = exp(0.1+0.4^2/2); eOutcome
 
 # Problem 2
+# Using the log utility of U(W) = log(W), evaluate two cases.
+# A
 w = 5000 
 unInsured = 0.5*log(w+1000) + 0.5*log(w-1000)
 insured = log(w-125)
 unInsured - insured # Positive utility, prefers not to buy insurance
 
+# B
 w = 4000
 unInsured= 0.5*log(w+1000) + 0.5*log(w-1000)
 insured = log(w-125)
 unInsured - insured # Negative utility, prefers to buy insurance
 
 # Problem 3
+# 10% fire that destroys the whole business.
+# 10% it destroys half her business.
+# 80% it destroys nothing.
+# Assume: U(W) = log(W)
 w = 100000
 eUtility = 0.1*log(w-99999) + 0.1*log(w-50000) + 0.8*log(w)
 
